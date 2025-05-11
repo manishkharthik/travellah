4cd25186d0fb472594651b7505ba917d1fd7bf54
manishkharthik <manishkharthik0630@gmail.com>
2025-05-11 23:06:57 +0800

    Initial commit

---
+++ my_app/backend/server.js
@@ -0,0 +1,15 @@
+// backend/server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Hello from backend'));
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(5000, () => console.log('Server running on port 5000')))
+  .catch(err => console.log(err));
